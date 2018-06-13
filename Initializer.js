015dc104a1ae51e7276241447a43ee3f0f5436e9
TheV0rtex <etienne.dmenard@gmail.com>
2018-06-13 17:56:39 -0400

    1.0.1 | Fixed little config generator mistake

--- Initializer.js
+++ Initializer.js
@@ -7,7 +7,7 @@ console.log("Initiating creation process.");
 if (!fs.existsSync(configPath)) { // config.json
     fs.writeFile(
         './config.json',
-        '{\n  "token":"",\n  "prefix":",\n}',
+        '{\n  "token":"",\n  "prefix":""\n}',
         function (configLog) {
         console.log('+ "./config.json" was created.');
     });
